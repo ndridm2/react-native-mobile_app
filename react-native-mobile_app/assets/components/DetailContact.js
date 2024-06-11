cbfdbe6f31a6fc3667779fac9c2beb6352a38897
Andri Darmuli <andri.darmuli26@gmail.com>
2024-06-11 07:32:11 +0700

    Add files via upload
    init: learn react native

---
+++ react-native-mobile_app/assets/components/DetailContact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+    View,
+    Text,
+  } from "react-native";
+
+const  DetailContact = () => {
+  return (
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+        <Text>Hello Word</Text>
+    </View>
+  )
+}
+
+export default DetailContact
